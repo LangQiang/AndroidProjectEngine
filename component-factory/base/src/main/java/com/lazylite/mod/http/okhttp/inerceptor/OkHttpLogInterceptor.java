e94ccf48437301586955d9c96dc72cbc065a8765
GodQ <lq910518@126.com>
2023-04-26 16:33:53 +0800

    okhttp 打印curl

--- component-factory/base/src/main/java/com/lazylite/mod/http/okhttp/inerceptor/OkHttpLogInterceptor.java
+++ component-factory/base/src/main/java/com/lazylite/mod/http/okhttp/inerceptor/OkHttpLogInterceptor.java
@@ -6,10 +6,17 @@ import com.lazylite.mod.http.mgr.KwHttpMgr;
 import com.lazylite.mod.http.okhttp.OkHttpConstants;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
 
 import okhttp3.Interceptor;
+import okhttp3.MediaType;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import timber.log.Timber;
 
 public class OkHttpLogInterceptor implements Interceptor {
 
@@ -27,6 +34,8 @@ public class OkHttpLogInterceptor implements Interceptor {
                 Log.i(OkHttpConstants.TAG, String.format("OkHttpLogInterceptor: %nSending request %s on %s%n%s",
                         request.url(), chain.connection(), request.headers()));
             }
+
+            printCurl(request);
 
             //2.网络请求
             Response response;
@@ -59,8 +68,9 @@ public class OkHttpLogInterceptor implements Interceptor {
                     Log.i(OkHttpConstants.TAG,
                             String.format("OkHttpLogInterceptor: %nReceived response for " + "%s " + "in " + "%.1f" + "ms%n" +
                                             "%s" + "%n" +
-                                            "Method: %s, ResponseType: %s%n%n ",
-                                    response.request().url(), (t2 - t1) / 1e6d, response.headers(), request.method(), responseType));
+                                            "Method: %s, ResponseType: %s%n%n"+
+                                            "Response Code：%s%n%n ",
+                                    response.request().url(), (t2 - t1) / 1e6d, response.headers(), request.method(), responseType,response.code()));
                 }
             }
 
@@ -72,4 +82,56 @@ public class OkHttpLogInterceptor implements Interceptor {
 
         }
     }
+
+    private void printCurl(Request request) {
+        String url = request.url().toString();
+        String method = request.method();
+        Map<String, List<String>> headers = request.headers().toMultimap();
+
+        StringBuilder curlSb = new StringBuilder();
+        curlSb.append("curl ").append(url);
+        curlSb.append(" -X ").append(method);
+
+        for (Map.Entry<String, List<String>> stringListEntry : headers.entrySet()) {
+            curlSb.append(" -H ");
+            curlSb.append("'");
+            curlSb.append(stringListEntry.getKey());
+            curlSb.append(":");
+            for (int i = 0; i < stringListEntry.getValue().size(); i++) {
+                curlSb.append(stringListEntry.getValue().get(i));
+                if (i != (stringListEntry.getValue().size() - 1)) {
+                    curlSb.append(",");
+                }
+            }
+            curlSb.append("'");
+        }
+
+        if ("POST".equalsIgnoreCase(method)) {
+            String body = getBody(request);
+            if (body != null) {
+                curlSb.append(" -d ").append("'").append(body).append("'");
+            }
+        }
+        String curlStr = curlSb.toString();
+        Timber.tag("okhttp").d(curlStr);
+    }
+
+    private String getBody(Request request) {
+        try {
+            RequestBody requestBody = request.body();
+            if (requestBody == null) return null;
+            okio.Buffer buffer = new okio.Buffer();
+            requestBody.writeTo(buffer);
+            Charset charset = StandardCharsets.UTF_8;;
+            MediaType contentType = requestBody.contentType();
+            if (contentType != null) {
+                charset = contentType.charset(StandardCharsets.UTF_8);
+            }
+
+            return buffer.readString(charset);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
 }
